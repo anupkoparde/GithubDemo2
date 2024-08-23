b8776d596b904d3d5140be13e95e3d8adaecadfe
LENOVO <LENOVO@DESKTOP-KE1MN2L>
2024-08-23 07:25:12 +0530

    "this my first commit"

---
+++ GithubDemo2/src/main/java/com/loginpage/LoginClass.java
@@ -0,0 +1,8 @@
+package com.loginpage;
+
+public class LoginClass {
+	public void login() {
+		System.out.println("this my fisrt commit");
+	}
+
+}
---
+++ GithubDemo2/src/test/java/com/testcase/LoginTest.java
@@ -0,0 +1,9 @@
+package com.testcase;
+
+public class LoginTest {
+	
+	public void logintest() {
+		System.out.println("First test case");
+	}
+
+}
